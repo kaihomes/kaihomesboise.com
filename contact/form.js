ee0bd81bbf54e2592cfac71b8f1a18db469e1bd7
David Hartsough <hartsoughdavid@gmail.com>
2026-03-04 19:19:59 -0700

    add turnstile

--- contact/form.js
+++ contact/form.js
@@ -8,17 +8,25 @@ const url = "https://eotxid8mqusel0u.m.pipedream.net";
 async function sendMessage(data = {}) {
   const headers = new Headers();
   headers.append("Content-Type", "application/json");
-  const response = await fetch(url, {
+  const body = JSON.stringify(data);
+  await fetch(url, {
     method: "POST",
     headers,
     mode: "cors",
-    body: JSON.stringify(data),
+    body,
   });
   return true;
 }
 
+function getTurnstileVal() {
+  // name="cf-turnstile-response" id="cf-chl-widget-ASDF_response"
+  // where ASDF is a random 5 char string
+  // get by name
+  return document.querySelector('input[name="cf-turnstile-response"]').value;
+}
+
 const getEl = (id) => document.getElementById(id);
-const getVal = (id) => getEl(id).value;
+const getVal = (id) => getEl(id).value.trim();
 const hide = (id) => {
   getEl(id).style.display = "none";
 };
@@ -31,6 +39,12 @@ const successDiv = getEl("success");
 const spinnerDiv = getEl("spinner");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  const turnstile = getTurnstileVal();
+  if (!turnstile) {
+    // TODO: add error message
+    alert("Please prove you're not a robot.");
+    return false;
+  }
   const address = `${getVal("address")}, ${getVal("city")}, ${getVal("zip")}`;
   const data = {
     name: getVal("name"),
@@ -38,6 +52,7 @@ form.addEventListener("submit", (e) => {
     phone: getVal("phone"),
     address,
     message: getVal("message"),
+    turnstile,
   };
   form.style.display = "none";
   loadingDiv.style.display = "block";
@@ -46,6 +61,7 @@ form.addEventListener("submit", (e) => {
     spinnerDiv.innerHTML = "";
     loadingDiv.style.display = "none";
     successDiv.style.display = "block";
+    if (window.turnstile.reset) turnstile.reset();
   });
   return false;
 });
